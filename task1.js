641ce2680f9d445ca4decb5d7aa300236d1f1615
Edison Chimezie <edison.chimezie01@gmail.com>
2022-06-24 03:10:25 +0100

    Javascript code that prints out your name, height, and country on the screen

--- task1.js
+++ task1.js
@@ -1,4 +1,8 @@
 /* Write a javascript code that prints out your name, height, and country on the screen. */
+
+
 console.log('Edison Chimezie');
+
 console.log('Height: 178cm');
+
 console.log('Country: Nigeria');
